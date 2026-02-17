1af8651ed0075cb68b674aa8872c805290b302d1
Suhail Dimmagudi <dimmagudisuhail502@gmail.com>
2026-02-17 18:25:20 +0530

    Cleaned movies views formatting (no logic change)

--- movies/views.py
+++ movies/views.py
@@ -5,9 +5,11 @@ from rest_framework.response import Response
 from .models import Movie, Watchlist
 from .serializers import MovieSerializer, WatchlistSerializer
 
+
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
@@ -15,25 +17,53 @@ def watchlist_view(request):
     if request.method == 'GET':
         watchlist = Watchlist.objects.filter(user=request.user)
         serializer = WatchlistSerializer(watchlist, many=True)
-        return Response(serializer.data)
-    
-    elif request.method == 'POST':
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'POST':
         movie_id = request.data.get('movie_id')
+
         try:
             movie = Movie.objects.get(id=movie_id)
-            watchlist, created = Watchlist.objects.get_or_create(user=request.user, movie=movie)
+            watchlist, created = Watchlist.objects.get_or_create(
+                user=request.user,
+                movie=movie
+            )
+
             if created:
-                return Response({'message': 'Added to watchlist'}, status=status.HTTP_201_CREATED)
-            return Response({'message': 'Already in watchlist'}, status=status.HTTP_200_OK)
+                return Response(
+                    {'message': 'Added to watchlist'},
+                    status=status.HTTP_201_CREATED
+                )
+
+            return Response(
+                {'message': 'Already in watchlist'},
+                status=status.HTTP_200_OK
+            )
+
         except Movie.DoesNotExist:
-            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {'error': 'Movie not found'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
 def remove_from_watchlist(request, movie_id):
     try:
-        watchlist = Watchlist.objects.get(user=request.user, movie_id=movie_id)
+        watchlist = Watchlist.objects.get(
+            user=request.user,
+            movie_id=movie_id
+        )
         watchlist.delete()
-        return Response({'message': 'Removed from watchlist'}, status=status.HTTP_200_OK)
+
+        return Response(
+            {'message': 'Removed from watchlist'},
+            status=status.HTTP_200_OK
+        )
+
     except Watchlist.DoesNotExist:
-        return Response({'error': 'Not in watchlist'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {'error': 'Not in watchlist'},
+            status=status.HTTP_404_NOT_FOUND
+        )
