9206c36932aa744b9116994841429d8821406831
Suhail Dimmagudi <dimmagudisuhail502@gmail.com>
2026-02-17 18:14:44 +0530

    Added related_name to Watchlist model for cleaner reverse access

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -15,9 +16,18 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 
+
 class Watchlist(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='watchlist'
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name='watchlisted_by'
+    )
     added_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
